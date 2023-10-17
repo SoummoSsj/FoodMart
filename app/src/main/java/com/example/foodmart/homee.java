ea3ffa41e1931db59973343b2a99ab787d554efa
SoummoSsj <81679223+SoummoSsj@users.noreply.github.com>
2023-10-17 21:17:14 +0600

    2nrepush

--- app/src/main/java/com/example/foodmart/homee.java
+++ app/src/main/java/com/example/foodmart/homee.java
@@ -9,6 +9,6 @@ public class homee extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homee);
+        setContentView(R.layout.activity_main);
     }
 }
